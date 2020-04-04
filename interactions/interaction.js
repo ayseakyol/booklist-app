99cc2e930aed2c44ff10ba543f2270cff2ccd487
ayseakyol <ayseakyol.22309@gmail.com>
2020-04-04 17:11:34 +0200

    interactions

---
+++ interactions/interaction.js
@@ -0,0 +1,31 @@
+// Event: Add a Book
+document.querySelector("#book-form").addEventListener("submit", (e) => {
+  debugger;
+  // Prevent actual submit
+  e.preventDefault();
+
+  // Get form values
+  const title = document.querySelector("#title").value;
+  const author = document.querySelector("#author").value;
+  const isbn = document.querySelector("#isbn").value;
+
+  // Validate
+  if (title === "" || author === "" || isbn === "") {
+    UI.showAlert("Please fill in all fields", "danger");
+  } else {
+    // Instantiate book
+    const book = new Book(title, author, isbn);
+
+    // Add Book to UI
+    UI.addBookToList(book);
+
+    // Add book to store
+    Store.addBook(book);
+
+    // Show success message
+    UI.showAlert("Book Added", "success");
+
+    // Clear fields
+    UI.clearFields();
+  }
+});
