78540351cd4053f4d8424de066ec04a892b652c4
ayseakyol <ayseakyol.22309@gmail.com>
2020-04-04 17:07:05 +0200

    app

---
+++ app/book.js
@@ -0,0 +1,20 @@
+// Book Class: Represents a Book
+class Book {
+  constructor(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+  }
+}
+
+// Event: Remove a Book
+document.querySelector("#book-list").addEventListener("click", (e) => {
+  // Remove book from UI
+  UI.deleteBook(e.target);
+
+  // Remove book from store
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+  // Show success message
+  UI.showAlert("Book Removed", "success");
+});
