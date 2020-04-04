82ee7f34dd6e019b440e9d3b5a45c6ebc5e88f83
Ayse Akyol <59367104+ayseakyol@users.noreply.github.com>
2020-04-04 21:42:38 +0200

    Update store.js

--- store.js
+++ store.js
@@ -1,5 +1,3 @@
-// Event: Display Books
-document.addEventListener("DOMContentLoaded", UI.displayBooks);
 // UI Class: Handle UI Tasks
 class UI {
   static displayBooks() {
@@ -80,4 +78,14 @@ class Store {
   }
 }
 
+// Event: Remove a Book
+document.querySelector("#book-list").addEventListener("click", (e) => {
+  // Remove book from UI
+  UI.deleteBook(e.target);
 
+  // Remove book from store
+  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+  // Show success message
+  UI.showAlert("Book Removed", "success");
+});
