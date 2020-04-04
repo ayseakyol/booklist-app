63c43bf954bc8c4d117203911eddcdacede4d695
Ayse Akyol <59367104+ayseakyol@users.noreply.github.com>
2020-04-04 21:45:01 +0200

    Update book.js

--- app/book.js
+++ app/book.js
@@ -7,14 +7,3 @@ class Book {
   }
 }
 
-// Event: Remove a Book
-document.querySelector("#book-list").addEventListener("click", (e) => {
-  // Remove book from UI
-  UI.deleteBook(e.target);
-
-  // Remove book from store
-  Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
-
-  // Show success message
-  UI.showAlert("Book Removed", "success");
-});
